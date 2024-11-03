934e9236a8c76241128e491e1da2355881777557
dmntrx <145675897+dmntrx@users.noreply.github.com>
2024-11-04 01:37:08 +0300

    Add files via upload

---
+++ dishes.js
@@ -0,0 +1,14 @@
+"use strict";
+
+export const dishes = [
+    { keyword: 'gazpacho', name: 'Гаспачо', price: 195, category: 'soup', count: '350 г', image: 'images/gazpacho.jpg' },
+    { keyword: 'mushroom', name: 'Грибной суп-пюре', price: 185, category: 'soup', count: '330 г', image: 'images/mushroom_soup.jpg' },
+    { keyword: 'norwegian', name: 'Норвежский суп', price: 270, category: 'soup', count: '330 г', image: 'images/norwegian_soup.jpg' },
+    { keyword: 'potato', name: 'Жареная картошка с грибами', price: 150, category: 'main', count: '250 г', image: 'images/potatomushrooms.jpg' },
+    { keyword: 'lasagna', name: 'Лазанья', price: 385, category: 'main', count: '310 г', image: 'images/lasagna.jpg' },
+    { keyword: 'cutlets', name: 'Котлеты из курицы с картофельным пюре', price: 225, category: 'main', count: '280 г', image: 'images/cutletpotato.jpg' },
+    { keyword: 'orange', name: 'Апельсиновый сок', price: 120, category: 'drink', count: '300 мл', image: 'images/orangejuice.jpg' },
+    { keyword: 'apple', name: 'Яблочный сок', price: 90, category: 'drink', count: '300 мл', image: 'images/applejuice.jpg' },
+    { keyword: 'carrot', name: 'Морковный сок', price: 110, category: 'drink', count: '300 мл', image: 'images/carrotjuice.jpg' }
+  ];
+  
